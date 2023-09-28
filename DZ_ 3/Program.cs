40b147f8848d8f1cbd3a935509d958f80019e0b8
IDaniil228 <99666220+IDaniil228@users.noreply.github.com>
2023-09-28 21:39:44 +0300

    Add project files.

---
+++ DZ_ 3/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DZ__3
+{
+    enum Mouth { Январь, Февраль, Март, Апрель, Май, Июнь, Июль, Август, Сентябрь, Октябрь, Ноябрь, Декабрь }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Упражнение 4.1 Написать программу, которая читает с экрана число и переводит этот число в месяц и день месяца.");
+            int[] daysOfMouth = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+            int[] daysOfMouthYear = { 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+            Console.Write("Введите год: ");
+            bool isNum_1 = int.TryParse(Console.ReadLine(), out int year);
+            Console.Write("Введите день: ");
+            bool isNum = int.TryParse(Console.ReadLine(), out int day);
+            string kindOfYear;
+            int sum = 0, secondSum = 0, i = 0;
+            if (isNum_1 && isNum)
+            {
+                if ((year % 4 == 0 && year % 100 == 0 && year % 400 == 0) || (year % 4 == 0 && year % 100 != 0))
+                {
+                    kindOfYear = "Високосный";
+                }
+                else
+                {
+                    kindOfYear = "Не високосный";
+                }
+                if (kindOfYear == "Не високосный")
+                {
+                    if (1 <= day && day <= 365)
+                    {
+                        while (sum < day)
+                        {
+                            secondSum = sum;
+                            sum += daysOfMouth[i];
+                            i++;
+                        }
+                        Console.WriteLine($"Число: {day - secondSum} \nМесяц: {(Mouth)i - 1}");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Нужно ввести число от 1 до 365");
+                    }
+                }
+                else
+                {
+                    if (1 <= day && day <= 366)
+                    {
+                        while (sum < day)
+                        {
+                            secondSum = sum;
+                            sum += daysOfMouthYear[i];
+                            i++;
+                        }
+                        Console.WriteLine($"Число: {day - secondSum} \nМесяц: {(Mouth)i - 1}");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Нужно ввести число от 1 до 366");
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("Нужно ввести корректные данные");
+
+            }
+            Console.WriteLine("Нажмите любую клавишу, чтобы закрыть приложение");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ DZ_3(2)/Program.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DZ_3_2_
+{
+    enum DaysOfWeek
+    {
+        Понедельник = 1,
+        Вторник = 2,
+        Среда = 3,
+        Четверг = 4,
+        Пятница = 5,
+        Суббота = 6,
+        Воскресенье = 7
+    }
+ 
+    //enum Cards
+    //{
+    //    Шестерка = 6,
+    //    Семерка = 7,
+    //    Восьмерка = 8,
+    //    Девятка = 9,
+    //    Десятка = 10,
+    //    Валет = 11,
+    //    Дама = 12,
+    //    Король = 13,
+    //    Туз = 14
+    //}
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Задание 1  последовательность из 10 чисел.");
+            Console.WriteLine("Введите 10 чисел через одну запятую");
+            string str = Console.ReadLine();
+            string[] numbers = str.Split(',');
+            double[] ints = new double[numbers.Length];
+            bool flag = true;
+            if (numbers.Length == 10)
+            {
+                for (int i = 0; i < numbers.Length; i++)
+                {
+                    bool isNum = Double.TryParse(numbers[i], out ints[i]);
+                    if (!isNum)
+                    {
+                        flag = false;
+                        Console.WriteLine("Вы ввели некорректные данные");
+                        break;
+                    }
+                }
+                if (flag)
+                {
+                    for (int i = 0; i < ints.Length - 1; i++)
+                    {
+                        if (ints[i] > ints[i + 1])
+                        {
+                            Console.WriteLine($"{i + 1} - индекс числа, который нарушает последовательность");
+                            flag = false;
+                            break;
+                        }
+                    }
+                }
+                if (flag)
+                {
+                    Console.WriteLine("Эта последовательность является упорядоченной по возрастанию");
+                }
+
+            }
+            else
+            {
+                Console.WriteLine("Нужно ввести 10 чисел через запятую!");
+            }
+
+            Console.WriteLine("Нажмите любую клавишу, чтобы закрыть приложение");
+            Console.ReadKey();
+
+            Console.WriteLine("Задача 2  Определить карту по номеру");// спросить почему не получилось с enum!!!!!!!
+            string[] cards = { "Шестерка", "Семерка", "Восьмерка", "Девятка", "Десятка", "Валет", "Дама", "Король", "Туз" };
+            while (true)
+            {
+                Console.Write("Введите номер карты от 6 до 14: ");
+                try
+                {
+                    int numb = Convert.ToInt32(Console.ReadLine()) - 6;
+                    Console.WriteLine($"Ваша карта - {cards[numb]}");
+                    break;
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Нужно ввести число");
+                }
+                catch (IndexOutOfRangeException)
+                {
+                    Console.WriteLine("Нужно ввести число от 6 до 14");
+                }
+            }
+            Console.WriteLine("Нажмите любую клавишу, чтобы закрыть приложение");
+            Console.ReadKey();
+
+            Console.WriteLine("Задание 3  Программа, которая принимает на входе строку и производит выходные данные");
+            Console.Write("Введите строку: ");
+            string str_1 = Console.ReadLine();
+            str_1 = str_1.ToLower();
+            switch (str_1)
+            {
+                case "jobroni":
+                    Console.WriteLine("Parton Tequila");
+                    break;
+                case "school counselor":
+                    Console.WriteLine("Anything with Alcohol");
+                    break;
+                case "programmer":
+                    Console.WriteLine("Hipster Craft Beer");
+                    break;
+                case "bike gang member":
+                    Console.WriteLine("Moonshine");
+                    break;
+                case "politician":
+                    Console.WriteLine("Your tax dollars");
+                    break;
+                case "rapper":
+                    Console.WriteLine("Cristal");
+                    break;
+                default:
+                    Console.WriteLine("Beer");
+                    break;
+
+            }
+            Console.WriteLine("Нажмите любую клавишу, чтобы закрыть приложение");
+            Console.ReadKey();
+
+            Console.WriteLine("Задание 4 День недели");
+            Console.Write("Введите номер дня недели:");
+            try
+            {
+                int number = Convert.ToInt32(Console.ReadLine());
+                if (1 <= number && number <= 7)
+                {
+                    Console.WriteLine($"День недели: {(DaysOfWeek)number}");
+                }
+                else
+                {
+                    Console.WriteLine("Нужно ввести цифру от 1 до 7");
+                }
+
+            }
+            catch
+            {
+                Console.WriteLine("Нужно ввести одну цифру!");
+            }
+            Console.WriteLine("Нажмите любую клавишу, чтобы закрыть приложение");
+            Console.ReadKey();
+
+            Console.WriteLine("Задание 5  При помощи foreach обойти весь массив.");
+            int sum = 0;
+            string[] strings = { "Hello Kitty", "123", "ytrewq", "Barbie doll", "QWERTY"}; 
+            foreach (string s in strings)
+            {
+                if (s == "Hello Kitty" || s == "Barbie doll")
+                {
+                    sum++;
+                }
+            }
+            Console.WriteLine($"Кол-во кукол в сумке: {sum}");
+            Console.WriteLine("Нажмите любую клавишу, чтобы закрыть приложение");
+            Console.ReadKey();
+        }
+    }
+}
